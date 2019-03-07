4969352cc1df00f1d4dea160623877ec7f86d953
Ty Hitzeman <tyler.hitzeman@gmail.com>
2019-03-07 17:10:07 -0600

    initial commit

---
+++ client/main.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Meteor } from "meteor/meteor";
+import { Tracker } from "meteor/tracker";
+
+import { Players, calculatePlayerPositions} from "./../imports/api/players";
+import App from './../imports/ui/App';
+
+import { func } from "prop-types";
+
+const renderPlayers = playersList => {
+  return playersList.map(player => {
+    return <Player key={player._id} player={player} />;
+  });
+};
+
+Meteor.startup(() => {
+  Tracker.autorun(() => {
+    let players = Players.find({}, {sort: {score: -1}}).fetch();
+    let positionedPlayers = calculatePlayerPositions(players);
+    let title = "Score Keep";
+    let subtitle = "Static subtitle";
+    ReactDOM.render(<App title={title} players={positionedPlayers}/>, document.getElementById("app"));
+  });
+
+  console.log(Players.find().fetch());
+});
